55f0314f11f464f29d88ba5ee99a6d1d6ad88935
modsiw <zzambbang@gmail.com>
2021-01-11 16:01:43 +0900

    springboot initialize

---
+++ ajaxtest/src/test/java/com/example/ajaxtest/AjaxtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ajaxtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjaxtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
